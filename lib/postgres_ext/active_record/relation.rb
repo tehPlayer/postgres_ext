6621ae312da7c325fb2f576e8b1db81790a3d51c
Scott Davis <sc@ttdavis.com>
2017-09-11 08:59:57 -0400

    change 5.1 names

--- lib/postgres_ext/active_record/relation.rb
+++ lib/postgres_ext/active_record/relation.rb
@@ -1,13 +1,13 @@
 gdep_4_2 = Gem::Dependency.new('activerecord', '>= 4.2.0')
 ar_4_2_version_cutoff = gdep_4_2.matching_specs.sort_by(&:version).last
 
-gdep_5_0 = Gem::Dependency.new('activerecord', '~> 5.1')
-ar_5_0_version_cutoff = gdep_5_0.matching_specs.sort_by(&:version).last
+gdep_5_1 = Gem::Dependency.new('activerecord', '~> 5.1')
+ar_5_1_version_cutoff = gdep_5_1.matching_specs.sort_by(&:version).last
 
 require 'postgres_ext/active_record/relation/merger'
 require 'postgres_ext/active_record/relation/query_methods'
 
-if ar_5_0_version_cutoff
+if ar_5_1_version_cutoff
   require 'postgres_ext/active_record/5.0/relation/predicate_builder/array_handler'
 elsif ar_4_2_version_cutoff
   require 'postgres_ext/active_record/relation/predicate_builder/array_handler'
